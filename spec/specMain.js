e506a31371f5bc6b30ea1b1d7cc899885f7ab260
naranjito72 <theclick36@gmail.com>
2021-06-13 22:15:45 +0200

    jasmine functions 2

--- spec/specMain.js
+++ spec/specMain.js
@@ -14,12 +14,28 @@ describe('findSameElementsArray', function(){
 
 describe('Returns', function(){
   it('Returns an array of numbers if there are different matches', function(){
-    // code
+    expect(findSameElementsArray).toHaveMethod('tellMeIfAnyMatch');
   });
   it('Returns only one number if there is only one match', function(){
-    // code
+    // findAllMatches()
+    expect(findSameElementsArray).toHaveMethod('findAllMatches')
   });
-  it('Returns "undefined" if there is not match', function(){
-    // code
-  })
-})
+  it('Returns one number despite having multiple matches', function(){
+    // findOnlyOne()
+    expect(findSameElementsArray).toHaveMethod('findOnlyOneMatch')
+  });
+});
+describe('Returns from different functions', function(){
+  // code
+  let arrNum = [1, 2, 3, 4, 5]
+const undefinedCases = [
+  {case: findSameElementsArray.tellMeIfAnyMatch(arrNum), answer: false},
+  {case: findSameElementsArray.findAllMatches(arrNum), answer: undefined},
+  {case: findSameElementsArray.findOnlyOneMatch(arrNu), answer: undefined}
+]
+undefinedCases.forEach((e, i) => {
+  it(`Returns "undefined" or "false" if there is not a match in the array case number${i + 1} return ${e.case}`, () => {
+    expect(e.case).toEqual(e.answer);
+  });
+});
+});
